98f6050f1532d30b560a8e1489d6cea6043c9f83
Darren Foong <darren.foong@transcelestial.com>
2024-01-03 16:22:08 +0800

    define DBG

--- mjpg-streamer-experimental/mjpg_streamer.h
+++ mjpg-streamer-experimental/mjpg_streamer.h
@@ -33,11 +33,7 @@
 #include <linux/videodev2.h>
 #include <pthread.h>
 
-#ifdef DEBUG
 #define DBG(...) fprintf(stderr, " DBG(%s, %s(), %d): ", __FILE__, __FUNCTION__, __LINE__); fprintf(stderr, __VA_ARGS__)
-#else
-#define DBG(...)
-#endif
 
 #define LOG(...) { char _bf[1024] = {0}; snprintf(_bf, sizeof(_bf)-1, __VA_ARGS__); fprintf(stderr, "%s", _bf); syslog(LOG_INFO, "%s", _bf); }
 
